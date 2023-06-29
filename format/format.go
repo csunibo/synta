0b52e45e71e4a2291229cff01b012cce1cc5c975
Luca <luca.tagliavini5@studio.unibo.it>
2023-06-29 19:49:53 +0200

    test format

--- format/format.go
+++ format/format.go
@@ -1,9 +1,23 @@
 package format
 
-import "github.com/csunibo/synta"
+import (
+	"sort"
 
-func Format(synta synta.Synta) (code string) {
-	for id, def := range synta.Definitions {
+	"github.com/csunibo/synta"
+)
+
+// Format formats a synta structure into a string which represnts the contents
+// of the associated synta file. The definitions are sorted by name. and blank
+// lines are let as per internal convention.
+func Format(syntaFile synta.Synta) (code string) {
+	definitions := []string{}
+	for id := range syntaFile.Definitions {
+		definitions = append(definitions, string(id))
+	}
+	sort.Strings(definitions)
+
+	for _, id := range definitions {
+		def := syntaFile.Definitions[synta.Identifier(id)]
 		for _, comment := range def.Comments {
 			code += "; " + comment + "\n"
 		}
@@ -11,18 +25,18 @@ func Format(synta synta.Synta) (code string) {
 	}
 
 	code += "> "
-	for i, segment := range synta.Filename.Segments {
+	for i, segment := range syntaFile.Filename.Segments {
 		if segment.Optional {
 			code += "(-" + string(segment.Identifier) + ")?"
 		} else {
 			code += string(segment.Identifier)
 		}
 
-		if i != len(synta.Filename.Segments)-1 {
+		if i != len(syntaFile.Filename.Segments)-1 && !syntaFile.Filename.Segments[i+1].Optional {
 			code += "-"
 		}
 	}
-	code += "." + string(synta.Filename.Extension) + "\n"
+	code += "." + string(syntaFile.Filename.Extension) + "\n"
 
 	return
 }
--- format/format_test.go
+++ format/format_test.go
@@ -24,3 +24,41 @@ test = c|d
 `
 	assert.Equal(t, formattedContent, formatted)
 }
+
+func TestFormatWithOptional(t *testing.T) {
+	basicContent := `def = a|b
+test = c|d
+> def(-test)?.test
+`
+	basicSynta, err := synta.ParseSynta(basicContent)
+	assert.Nil(t, err)
+
+	formatted := Format(basicSynta)
+	formattedContent := `def = a|b
+
+test = c|d
+
+> def(-test)?.test
+`
+	assert.Equal(t, formattedContent, formatted)
+}
+
+func TestFormatWithComments(t *testing.T) {
+	basicContent := `; a test comment
+def = a|b
+test = c|d
+> def(-test)?.test
+`
+	basicSynta, err := synta.ParseSynta(basicContent)
+	assert.Nil(t, err)
+
+	formatted := Format(basicSynta)
+	formattedContent := `; a test comment
+def = a|b
+
+test = c|d
+
+> def(-test)?.test
+`
+	assert.Equal(t, formattedContent, formatted)
+}
