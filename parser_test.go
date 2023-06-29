d8fb3a16aceb8e0311e8c73086c2a9a39450fc33
konova8 <canovasimo@gmail.com>
2023-06-29 13:14:11 +0200

    Passed all initial tests

--- parser_test.go
+++ parser_test.go
@@ -1,11 +1,17 @@
 package synta
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+type DefinitionString struct {
+	Comments     []string
+	RegexpString string
+}
 
 func TestParseSyntaWithEmptyFile(t *testing.T) {
 	synta, err := ParseSynta("")
@@ -20,9 +26,18 @@ func TestParseSyntaWithSingleDefinition(t *testing.T) {
 	synta, err := ParseSynta(input)
 	assert.Nil(t, err)
 	assert.NotEmpty(t, synta.Definitions)
-	def := Definition{Comments: []string{}, Regexp: regexp.MustCompile("a|b")}
-	assert.Contains(t, synta.Definitions, def)
+
+	def := map[Identifier]DefinitionString{}
+	def["test"] = DefinitionString{RegexpString: regexp.MustCompile("a|b").String()}
+	defParsed := map[Identifier]DefinitionString{}
+	for id, d := range synta.Definitions {
+		fmt.Printf("Checking for key `%s`\n", id)
+		defParsed[id] = DefinitionString{d.Comments, d.Regexp.String()}
+		assert.Equal(t, def[id], defParsed[id])
+	}
 
 	assert.NotEmpty(t, synta.Filename)
 	// TODO: controllare anche che filename contenga le strutture appropriate
+	seg := Segment{Identifier: "test", Optional: false}
+	assert.Contains(t, synta.Filename, seg)
 }
