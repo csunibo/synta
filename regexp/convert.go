85161526d6d48ec4977349c51653a99a6919a083
Luca <luca.tagliavini5@studio.unibo.it>
2023-07-05 16:55:05 +0200

    add support to generate the regexp just for the filename

--- regexp/convert.go
+++ regexp/convert.go
@@ -8,32 +8,43 @@ import (
 )
 
 func Convert(synta synta.Synta) (expr *regexp.Regexp, err error) {
-    var finalString string = ""
-    for i, segment := range synta.Filename.Segments {
-        definition, isPresent := synta.Definitions[segment.Identifier]
+	filenameRegexp, err := ConvertWithoutExtension(synta)
+	if err != nil {
+		return
+	}
+	finalString := filenameRegexp.String()
 
-        if !isPresent {
-            err = fmt.Errorf("Missing definition for %s", segment.Identifier)
-            return
-        }
+	finalString += "\\.(" + synta.Definitions[synta.Filename.Extension].Regexp.String() + ")"
+	expr, err = regexp.Compile(finalString)
 
-        if segment.Optional {
-            finalString += "(-(" + definition.Regexp.String() + "))?" 
-        } else {
-            finalString += "(" + definition.Regexp.String() + ")"
-        }
+	// Simplify when we use regexp/syntax
+	// if err == nil {
+	//     expr = expr.Simplify()
+	// }
+	return
+}
 
-        if i != len(synta.Filename.Segments) - 1 && !synta.Filename.Segments[i+1].Optional {
-            finalString += "-"
-        }
-    }
+func ConvertWithoutExtension(synta synta.Synta) (expr *regexp.Regexp, err error) {
+	var finalString string = ""
+	for i, segment := range synta.Filename.Segments {
+		definition, isPresent := synta.Definitions[segment.Identifier]
 
-    finalString += "\\.(" + synta.Definitions[synta.Filename.Extension].Regexp.String() + ")"
-    expr, err = regexp.Compile(finalString)
+		if !isPresent {
+			err = fmt.Errorf("Missing definition for %s", segment.Identifier)
+			return
+		}
 
-    // Simplify when we use regexp/syntax
-    // if err == nil {
-    //     expr = expr.Simplify()
-    // }
+		if segment.Optional {
+			finalString += "(-(" + definition.Regexp.String() + "))?"
+		} else {
+			finalString += "(" + definition.Regexp.String() + ")"
+		}
+
+		if i != len(synta.Filename.Segments)-1 && !synta.Filename.Segments[i+1].Optional {
+			finalString += "-"
+		}
+	}
+
+	expr, err = regexp.Compile(finalString)
 	return
 }
